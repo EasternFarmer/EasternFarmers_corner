d416144b3238077f7efc41de258f363e018ed75a
EasternFarmer <Franek.banas1@gmail.com>
2025-07-11 00:20:35 +0200

    .

--- src/colors.lua
+++ src/colors.lua
@@ -2,7 +2,8 @@ G.C.EF = {
     PLANT = HEX("199e19"),
 }
 
-local loc_colour_ref = loc_colour
+-- https://github.com/nh6574/JoyousSpring/blob/main/src/globals.lua#L71
+local loc_colour_ref = loc_colour 
 ---@diagnostic disable-next-line: lowercase-global
 function loc_colour(_c, _default)
     if not G.ARGS.LOC_COLOURS then
